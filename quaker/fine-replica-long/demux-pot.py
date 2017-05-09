48144ac6a5e905a700a5033f3df31216d0bf2fe5
Pratima Satish <pratimasatish@berkeley.edu>
2017-05-09 11:40:18 -0700

    add demuxing scripts for energies and potential energies as well

---
+++ quaker/fine-replica-long/demux-pot.py
@@ -0,0 +1,44 @@
+from __future__ import print_function
+import numpy as np
+
+log = np.genfromtxt('log.stripped').astype(int)
+
+filename = ["pot-349.0.txt",
+	    "pot-349.5.txt",
+	    "pot-350.0.txt",
+	    "pot-350.5.txt",
+	    "pot-351.0.txt",
+	    "pot-351.5.txt"]
+
+newfiles = ["pot-new.349.0.txt",
+	    "pot-new.349.5.txt",
+	    "pot-new.350.0.txt",
+	    "pot-new.350.5.txt",
+	    "pot-new.351.0.txt",
+	    "pot-new.351.5.txt"]
+
+openfiles = []
+
+for f in filename:
+    openfiles.append(open(f, 'r'))
+
+first_time = True
+for row in log:
+    time = row[0]
+    if (time%1000 != 0):
+        continue
+    print(time)
+    for i,f in enumerate(openfiles):
+        print(i, row[i+1])
+        lines = f.readline()
+        if first_time:
+            g = open(newfiles[row[i+1]], 'w+')
+        else:
+            g = open(newfiles[row[i+1]], 'a')
+	g.write(lines)
+        g.close()
+    first_time=False
+
+for f in openfiles:
+    f.close()
+
