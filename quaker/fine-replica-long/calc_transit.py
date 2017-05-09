954e3278750cbd71c4b68703f6decb906f074147
Pratima Satish <pratimasatish@berkeley.edu>
2017-05-09 16:20:41 -0700

    think i succeeded in getting MBAR to work?

---
+++ quaker/fine-replica-long/calc_transit.py
@@ -0,0 +1,20 @@
+from __future__ import print_function
+import numpy as np
+
+log = np.genfromtxt('log.stripped').astype(int)
+t_max = [None, None, None, None, None, None]
+tdist = []
+
+for row in log:
+    time = row[0] - 1
+    print(time)
+    minint = row[1]
+    maxint = row[6]
+    if t_max[maxint] is None:
+        t_max[maxint] = time
+    if not t_max[minint] is None:
+        tdist.append(time - t_max[minint])
+        t_max[minint] = None
+
+print t_max
+
