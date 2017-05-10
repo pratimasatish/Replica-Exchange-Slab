88532dde4be8b7bdfad4f0ae26801494aa3bf711
Pratima Satish <pratimasatish@berkeley.edu>
2017-05-10 12:01:05 -0700

    still finding transition temperature

--- quaker/fine-replica-long/plot-en-vs-temp.py
+++ quaker/fine-replica-long/plot-en-vs-temp.py
@@ -9,18 +9,35 @@ def read_file(filename):
 
 temperature_k = [349.0, 349.5, 350.0, 350.5, 351.0, 351.5]
 K = len(temperature_k)
-T = 501
+T = 1001
 
-U_kn = np.zeros((K, T))		# U_kn[k,t] is the potential energy (in kcal/mol) for snapshot t of temperature index k
+E_kn = np.zeros((K, T))		# U_kn[k,t] is the potential energy (in kcal/mol) for snapshot t of temperature index k
 for k in range(K):
     lines = read_file("en-new.{:3.1f}.txt".format(temperature_k[k]))
-    U_kn[k, :] = lines[1500:]
+    E_kn[k, :] = lines[1000:]
 
-U_kn = np.array(U_kn)
+E_kn = np.array(E_kn)
 
-avgE = np.mean(U_kn, axis=1)
+avgE = np.mean(E_kn, axis=1)
 # print avgE.shape
 
 plt.figure(0)
-plt.plot(temperature_k, avgE, 'o')
+plt.plot(temperature_k, avgE, 'o', markersize=20)
+plt.show()
+
+
+theta_kn = np.zeros((K, T))		# U_kn[k,t] is the potential energy (in kcal/mol) for snapshot t of temperature index k
+for k in range(K):
+    theta_k = np.genfromtxt("theta{:3.1f}.txt".format(temperature_k[k]))
+    theta_k = theta_k.reshape((-1, 240))
+    theta_lines = np.mean(theta_k, axis=1)
+    theta_kn[k, :] = theta_lines[1000:]
+
+theta_kn = np.array(theta_kn)
+
+avgtheta = np.mean(theta_kn, axis=1)
+# print avgtheta.shape
+
+plt.figure(1)
+plt.plot(temperature_k, avgtheta, 'o', markersize=20)
 plt.show()
