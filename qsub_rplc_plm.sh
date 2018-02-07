8c61d63eb3a47843cc13a872aea776088ca2b25a
Pratima Satish <pratimasatish@berkeley.edu>
2018-02-07 15:32:20 -0800

    added files to analyse bias replica exchnage in vacuum and get free energy profiles

--- qsub_rplc_plm.sh
+++ qsub_rplc_plm.sh
@@ -1,4 +1,6 @@
 #!/bin/bash --login
 
 # mpirun -np 6 lmp_rplc_plm -p 6x1 -in in.replica -screen none
-mpirun -np 2 lmp_rplc_plm -p 2x1 -in in.rplc_plm 
+# mpirun -np 2 lmp_rplc_plm -p 2x1 -in in.rplc_plm 
+mpirun -np 2 ~/Downloads/lammps-2Apr13-replica/src/lmp_openmpi -p 2x1 -in in.rplc_plm 
+
